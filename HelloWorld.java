53fd4ec2d3d111221c92d62036506338fc09a80c
thetkhine <macbook@MacBooks-MacBook-Pro.local>
2023-02-24 10:52:19 +0630

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+class HelloWorld
+{
+    public static void main(String[]args)
+    {
+        int a = 10;
+        System.out.println("A is "+a);
+
+        a ++;
+
+        a = false;
+        boolean flag = true;
+        //flag ++;
+    }
+}
---
+++ hello.ts
@@ -0,0 +1,4 @@
+let a = 3;
+console.log('Type of a',typeof(a));
+
+//a = "Hello";
